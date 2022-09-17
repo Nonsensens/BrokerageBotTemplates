c12f8e14adefba5d791600a6c1f6f40da66ca270
Nonsensens <yarygi66@gmail.com>
2022-09-17 20:49:24 +0300

    fdsfd

--- script_reg.js
+++ script_reg.js
@@ -10,6 +10,17 @@ document.addEventListener('DOMContentLoaded', () => {
         let username = document.getElementById('username').value;
         let password = document.getElementById('password').value;
         let email = document.getElementById('email').value;
+        const requestUrl = 'http://127.0.0.1:5000/registration';
+        const xhr = new XMLHttpRequest();
+        xhr.open('POST', requestUrl);
+        xhr.setRequestHeader('Content-type', 'application/json; charset=UTF-8');
+        let data = {
+            username: username,
+            password: password,
+            email: email
+        }
+        xhr.send(JSON.stringify(data))
+
         webApp.close();
     })
 }
