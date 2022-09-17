0597c627b6d4e708f10dee17939f8ed7fe7ef71d
Nonsensens <yarygi66@gmail.com>
2022-09-17 21:00:22 +0300

    fdsf

--- script_reg.js
+++ script_reg.js
@@ -6,6 +6,22 @@ document.addEventListener('DOMContentLoaded', () => {
         'color': '#F4D03F',
         'textColor': "#black",
     });
+    let bt = document.getElementById('bt_test');
+    bt.addEventListener('click', () => {
+        let username = document.getElementById('username').value;
+        let password = document.getElementById('password').value;
+        let email = document.getElementById('email').value;
+        const requestUrl = 'http://127.0.0.1:5000/registration';
+        const xhr = new XMLHttpRequest();
+        xhr.open('POST', requestUrl);
+        xhr.setRequestHeader('Content-type', 'application/json; charset=UTF-8', 'Access-Control-Allow-Origin', 'http://127.0.0.1:5000/registration');
+        let data = {
+            username: username,
+            password: password,
+            email: email
+        }
+        xhr.send(JSON.stringify(data))
+    })
     Telegram.WebApp.onEvent('mainButtonClicked', () =>{
         let username = document.getElementById('username').value;
         let password = document.getElementById('password').value;
