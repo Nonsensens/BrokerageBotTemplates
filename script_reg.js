5575c2a172bf9c1f76fce8e8c1376dd1ac555a88
Nonsensens <yarygi66@gmail.com>
2022-09-17 19:45:18 +0300

    jsss

--- script_reg.js
+++ script_reg.js
@@ -1,6 +1,10 @@
 window.onload = function(){
     let webApp = window.Telegram.WebApp
     webApp.MainButton.isVisible = 'true';
+    webApp.MainButton.setParams({
+        'text': 'Регистрация',
+        'color': '00000'
+    })
     webApp.MainButton.onClick = () => {
         webApp.close();
     };
