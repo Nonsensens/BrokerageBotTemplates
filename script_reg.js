ce9544e6c22d7abf2d00f407291a7725d33cef0d
Nonsensens <yarygi66@gmail.com>
2022-09-17 19:21:56 +0300

    but fix

--- script_reg.js
+++ script_reg.js
@@ -3,5 +3,6 @@ window.onload = function(){
     let but = document.getElementById("button_reg");
     but.onclick = function(){
         webApp.close()
+        but.classList.toggle('ds')
     };
 };
