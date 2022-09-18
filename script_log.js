d347902dda0263458beec639f4143ad12901074f
Nonsensens <yarygi66@gmail.com>
2022-09-18 13:30:52 +0300

    BIG FIX LOG

--- script_log.js
+++ script_log.js
@@ -1,7 +1,26 @@
-window.onload = function(){
-    let webApp = window.Telegram.WebApp
-    let but = document.getElementById("button_reg");
-    but.onclick = function(){
-        webApp.close()
-    };
-};
+document.addEventListener('DOMContentLoaded', () => {
+    let webApp = window.Telegram.WebApp;
+    webApp.MainButton.isVisible = 'true';
+    webApp.MainButton.setParams({
+        'text': 'Регистрация',
+        'color': '#F4D03F',
+        'textColor': '#region',
+    });
+    Telegram.WebApp.onEvent('mainButtonClicked', () =>{
+        let username = document.getElementById('username').value;
+        let password = document.getElementById('password').value;
+        const requestUrl = 'http://127.0.0.1:5000/login';
+        const xhr = new XMLHttpRequest();
+        xhr.open('POST', requestUrl);
+        xhr.setRequestHeader('Content-type', 'application/json; charset=UTF-8');
+        let data = {
+            tg_user_id: 750899598,
+            username: username,
+            password: password,
+            email: email,
+        };
+        xhr.send(JSON.stringify(data));
+        webApp.close();
+    })
+}
+);
